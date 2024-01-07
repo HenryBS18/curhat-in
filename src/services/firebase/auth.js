2335b9feea3e1f961596484e88fbdf580b9a17f0
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-01-07 23:54:37 +0800

    Create Firebase Authentication Functions

---
+++ src/services/firebase/auth.js
@@ -0,0 +1,28 @@
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
+export const login = async (email, password) => {
+  const response = await signInWithEmailAndPassword(auth, email, password).then((res) => res.user);
+  return response
+}
+
+export const register = async (email, password) => {
+  const response = await createUserWithEmailAndPassword(auth, email, password);
+  return response
+}
+
+export const logout = () => {
+  signOut(auth).then(() => {
+    console.log('logged out');
+    return true
+  })
+}
+
+export const checkAuth = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      return user
+    }
+    return null
+  })
+}
