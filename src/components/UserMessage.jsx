2530806b9776e3aa48eb6c5362d748abc47db12d
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-01-15 00:24:18 +0800

    Create UserMessage

---
+++ src/components/UserMessage.jsx
@@ -0,0 +1,15 @@
+const UserMessage = ({ message }) => {
+  return (
+    <div className="w-full bg-[#FFD8A9] flex justify-center">
+      <div className="w-[90%] flex justify-between py-3">
+        <div className="w-[48px] h-[48px] bg-white rounded-full flex justify-center items-center">
+          <img src="/assets/icons/user-icon.svg" alt="user" width={24} height={24} />
+        </div>
+
+        <p className="w-[94%] text-lg font-semibold">{message}</p>
+      </div>
+    </div>
+  )
+}
+
+export default UserMessage
