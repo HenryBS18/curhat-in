1e9e12752e6181c5d5314165bf3b57f43457987c
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-15 23:07:49 +0800

    Move EducationDetail to pages folder

--- src/pages/EducationDetail.jsx
+++ src/pages/EducationDetail.jsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 
 const EducationDetail = () => {
   const params = useParams();
+
   const educationData = [
     {
       id: 1,
@@ -16,10 +17,10 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
     {
       id: 2,
@@ -33,10 +34,10 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
     {
       id: 3,
@@ -50,10 +51,10 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
     {
       id: 4,
@@ -67,10 +68,10 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
     {
       id: 5,
@@ -84,10 +85,10 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
     {
       id: 6,
@@ -101,64 +102,65 @@ const EducationDetail = () => {
         "Untuk meningkatkan kesejahteraan secara keseluruhan, remaja tidak bisa hanya berfokus pada kesehatan fisik saja, karena kesehatan mental juga memainkan peran yang besar dalam kehidupan. Kesehatan mental menunjukkan kemampuan diri sendiri untuk mengelola perasaan dan menghadapi kesulitan sehari-hari.",
       desc3:
         "Beberapa langkah perubahan sederhana bisa diterapkan untuk meningkatkan kualitas kesehatan mental. Dengan melakukan perubahan ini, maka dapat dipastikan hal ini memengaruhi semua aspek kehidupan. Nah, untuk remaja yang merasa mudah mengalami stress atau gangguan kecemasan, berikut ini beberapa hal sederhana yang dapat meningkatkan kesehatan mental:",
-      desc4: "• Katakan Hal Positif pada Diri Sendiri",
+      desc4: "• Katakan Hal Positif pada Diri Sendiri",
       exp1: "Penelitian menunjukkan bahwa cara kamu berpikir tentang diri sendiri dapat memiliki efek yang kuat pada kejiwaan kamu. Ketika kita memandang diri kita dan hidup kita secara negatif, maka kita juga merasakan efek negatifnya. Sebaliknya, jika membiasakan diri menggunakan kata-kata yang membuat lebih positif, maka hal ini membuat kamu lebih optimis.",
-      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
-      exp2:"Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
+      desc5: "• Tuliskan Hal-Hal yang Patut Disyukuri",
+      exp2: "Rasa bersyukur dikaitkan dengan peningkatan kesejahteraan, kualitas kesehatan mental, serta kebahagiaan. Cara sederhana untuk meningkatkan rasa bersyukur adalah membuat jurnal dan menuliskan berbagai hal yang patut disyukuri setiap harinya. Secara umum merenungkan rasa terimakasih juga efektif, tetapi kamu perlu berlatih secara teratur untuk mendapatkan manfaat jangka panjang. Temukan sesuatu untuk disyukuri dan nikmati perasaan tersebut dalam hatimu.",
     },
   ];
-  console.log(params);
 
-  const Data = educationData.find((eduacation) => eduacation.id == params.id);
-  console.log(Data);
+  const data = educationData.find((eduacation) => eduacation.id == params.id);
+
   return (
     <div className="bg-[#FFF0DE] w-full flex items-center flex-col">
       <div className="w-[90%] min-h-[775px]">
         <Navbar />
+
         <div className="mt-12">
           <img
-            src={Data.image}
-            alt={Data.title}
+            src={data.image}
+            alt={data.title}
             className="w-full h-[300px] object-cover mb-4 rounded-2xl"
           />
+
           <h3 className="text-[36px] font-bold mb-2 text-[#E38B29] text-ellipsis">
-            {Data.title}
+            {data.title}
           </h3>
 
           <h3 className="text-base font-semibold mb-2 text-black opacity-30 text-ellipsis">
-            {Data.author}
+            {data.author}
           </h3>
 
           <h3 className="text-base font-semibold mb-4 text-black opacity-30 text-ellipsis">
-            {Data.date}
+            {data.date}
           </h3>
 
           <h3 className="text-[14px] mb-3 text-black text-ellipsis">
-            {Data.desc1}
+            {data.desc1}
           </h3>
 
           <h3 className="text-[14px] mb-3 text-black text-ellipsis">
-            {Data.desc2}
+            {data.desc2}
           </h3>
 
           <h3 className="text-[14px] mb-3 text-black text-ellipsis">
-            {Data.desc3}
+            {data.desc3}
           </h3>
 
           <h3 className="text-[14px] font-bold mb-3 text-black text-ellipsis">
-            {Data.desc4}
+            {data.desc4}
           </h3>
 
           <h3 className="text-[14px] mb-3 text-black text-ellipsis">
-            {Data.exp1}
+            {data.exp1}
           </h3>
 
           <h3 className="text-[14px] font-bold mb-3 text-black text-ellipsis">
-            {Data.desc5}
+            {data.desc5}
           </h3>
 
           <h3 className="text-[14px] mb-3 text-black text-ellipsis">
-            {Data.exp2}
+            {data.exp2}
           </h3>
         </div>
       </div>
