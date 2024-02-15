b4f15a779a2eb76ea01386668844aaaf2df859dc
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-15 23:08:24 +0800

    Create routes config

---
+++ src/routes/index.jsx
@@ -0,0 +1,65 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import LoginReminder from '../pages/LoginReminder';
+import Home from '../pages/Home';
+import Education from '../pages/Education';
+import Chatbot from '../pages/Chatbot';
+import Konsultasi from '../pages/Konsultasi';
+import Quiz from '../pages/Quiz';
+import QuizDetail from '../pages/QuizDetail';
+import QuizFinished from '../pages/QuizFinished';
+import EducationDetail from '../pages/EducationDetail';
+import NotFound from '../pages/NotFound';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login-reminder',
+    element: <LoginReminder />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/materi-edukasi',
+    element: <Education />,
+  },
+  {
+    path: '/chatbot',
+    element: <Chatbot />,
+  },
+  {
+    path: '/materi-edukasi/:id',
+    element: <EducationDetail />,
+  },
+  {
+    path: '/layanan-konsultasi',
+    element: <Konsultasi />,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz />
+  },
+  {
+    path: "/quiz/:id",
+    element: <QuizDetail />
+  },
+  {
+    path: "/quiz/:id/score",
+    element: <QuizFinished />
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  }
+]);
