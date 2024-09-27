05070f8d8b47c0953c61807aaa3f393c63f5f16a
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-09-27 19:49:17 +0800

    Update initial prompt

--- src/store/useChatbotStore.ts
+++ src/store/useChatbotStore.ts
@@ -4,11 +4,11 @@ import { Chat, ChatbotStore } from "../types"
 const initialChats: Chat[] = [
   {
     from: "user",
-    message: "hai, tolong jawab pertanyaan-pertanyaan saya seperti seorang psikolog. berpura-puralah menjadi seorang psikolog. gunakan bahasa yang tidak terlalu formal",
+    message: "hai, tolong jawab pertanyaan-pertanyaan saya seperti seorang psikolog. berpura-puralah menjadi seorang psikolog. gunakan bahasa yang tidak terlalu formal. tolong jangan pernah menjawab diluar konteks kamu sebagai seorang psikolog. jika saya bertanya hal lain diluar bidang yang kamu kuasai, yaitu psikologi, tolong tolak pertanyaan itu dengan baik.",
   },
   {
     from: "model",
-    message: "baiklah saya akan berpura-pura dan menjawab seperti seorang psikolog.",
+    message: "baiklah saya akan berpura-pura dan menjawab seperti seorang psikolog. saya tidak akan menjawab diluar konteks ilmu saya yaitu psikologi. saya akan berbicara dengan santai seperti teman sehingga kamu nyaman.",
   }
 ]
 
